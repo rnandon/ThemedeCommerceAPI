557a2af9ee935a9a2a0919909a7b49acb1616f22
natemcfaddenDev <natemcfadden23@gmail.com>
2021-08-31 15:54:53 -0400

    Updated Reviews Controller

---
+++ eCommerceStarterCode/Controllers/ReviewsController.cs
@@ -0,0 +1,38 @@
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/reviews")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ReviewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var reviews = _context.Reviews;
+            return Ok(reviews);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Review value)
+        {
+            _context.Reviews.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+
+
+    }
+}
