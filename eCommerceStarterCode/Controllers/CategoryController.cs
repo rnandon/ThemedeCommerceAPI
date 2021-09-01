c1da952681cb4f91a66c9a47607ffd16c109bcf8
kschao <koyschao@icloud.com>
2021-09-01 10:19:57 -0500

    fixed category controller

---
+++ eCommerceStarterCode/Controllers/CategoryController.cs
@@ -0,0 +1,60 @@
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // <baseurl>/api/categories
+        [HttpGet]
+        public IActionResult GetAllCategories()
+        {
+            var categories = _context.Categories;
+            return Ok(categories);
+        }
+
+
+        // <baseurl>/api/category/1
+        [HttpGet("{id}")]
+        public IActionResult GetCategoryId(int id)
+        {
+            var categories = _context.Categories;
+            if (categories == null)
+            {
+                return NotFound();
+            }
+            return Ok(categories);
+        }
+        [HttpPost]
+        public IActionResult NewCategory([FromBody] Category value)
+        {
+            _context.Categories.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCategory(int id)
+        {
+            var categoryToDelete = _context.Categories.Find(id);
+            if (categoryToDelete == null)
+            {
+                return NotFound();
+            }
+            _context.Categories.Remove(categoryToDelete);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+
+}
+        
---
+++ eCommerceStarterCode/Controllers/OrderController.cs
@@ -0,0 +1,55 @@
+﻿using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Exchange.WebServices.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrdersController : ControllerBase
+{
+    private readonly AppContext appcontext;
+    private ResponseObject response;
+
+    public OrderController(AppContext appDbContext)
+    {
+        this.appcontext = appDbContext;
+        response = new ResponseObject();
+        response.Status = false;
+    }
+
+    // GET: api/Orders
+    [HttpGet]
+    public async Task<ResponseObject> Get()
+    {
+        await HttpContext.Session.LoadAsync();
+        var userId = HttpContext.Session.GetInt32("UserID");
+        if (HttpContext.Session.GetInt32("IsLoggedIn") == 1)
+        {
+            List<Order> orders = await appcontext.Orders
+                .Where(o => o.UserId == userId)
+                .Include(oi => oi.OrderItems)
+                .ToListAsync();
+            response.SetContent(true, "Order history fetched successfully", orders.ToList<object>());
+        }
+        else
+        {
+            response.Status = false;
+            response.Message = "Unauthorized access not allowed";
+        }
+        return response;
+    }
+    internal class ResponseObject
+    {
+        internal bool Status;
+
+        public string Message { get; internal set; }
+
+        internal void SetContent(bool v1, string v2, List<object> list)
+        {
+            throw new NotImplementedException();
+        }
+    }
