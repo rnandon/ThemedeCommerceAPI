c06ac30e951824b51e8433a72b0c7e1a14b53606
craigrmills <craig.r.mills86@gmail.com>
2021-09-01 16:23:34 -0500

    Added Authorization to CartController

--- eCommerceStarterCode/Controllers/CartController.cs
+++ eCommerceStarterCode/Controllers/CartController.cs
@@ -21,51 +21,75 @@ namespace eCommerceStarterCode.Controllers
             _context = context;
         }
 
-        [HttpGet]
+        [HttpGet, Authorize(Roles = "Admin")]
         public IActionResult GetAllCarts()
         {
             var carts = _context.Carts;
             return Ok(carts);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}"), Authorize]
         public IActionResult GetCartbyId(int id)
         {
-            var cart = _context.Carts.Find(id);
-            if (cart == null)
+            string userId = User.FindFirstValue("id");
+            User currentUser = _context.Users.Find(userId);
+            Cart cart = _context.Carts.Find(id);
+            if (cart == null || currentUser == null)
             {
                 return NotFound();
+            }
+            else if (cart.UserId != currentUser.Id)
+            {
+                return Unauthorized();
             }
             return Ok(cart);
         }
 
-        [HttpGet("user/{userId}")]
-        public IActionResult GetCartbyUser(string userId)
+        [HttpGet("user/{userId}"), Authorize]
+        public IActionResult GetCartbyUser(string id)
         {
-            var user = _context.Users.Find(userId);
-            var userCart = _context.Carts.Where(x => x.User == user);
-            if (user == null)
+            string userId = User.FindFirstValue("id");
+            User currentUser = _context.Users.Find(userId);
+            User user = _context.Users.Find(id);
+            if (user == null || currentUser == null)
             {
                 return NotFound();
             }
+            else if (user != currentUser)
+            {
+                return Unauthorized();
+            }
+            var userCart = _context.Carts.Where(x => x.User == user);
             return Ok(userCart);
         }
 
-        [HttpPost]
+        [HttpPost, Authorize]
         public IActionResult NewCart([FromBody] Cart value)
         {
+            string userId = User.FindFirstValue("id");
+            User currentUser = _context.Users.Find(userId);
+            if (currentUser == null)
+            {
+                return Unauthorized();
+            }
             _context.Carts.Add(value);
             _context.SaveChanges();
             return StatusCode(201, value);
         }
 
-        [HttpPut]
+        [HttpPut, Authorize]
         public IActionResult UpdateCart([FromBody] Cart value)
         {
+            string userId = User.FindFirstValue("id");
+            User currentUser = _context.Users.Find(userId);
             Cart cartToChange = _context.Carts.Find(value.CartId);
-            if (cartToChange == null)
+            if (cartToChange == null || currentUser == null)
             {
                 return NotFound();
+            }
+            else if (cartToChange.UserId != currentUser.Id)
+            {
+                return Unauthorized();
             }
             cartToChange.UserId = value.UserId;
             cartToChange.ProductId = value.ProductId;
@@ -75,13 +99,19 @@ namespace eCommerceStarterCode.Controllers
             return Ok(value);
         }
 
-        [HttpDelete("{id}")]
+        [HttpDelete("{id}"), Authorize]
         public IActionResult DeleteCart(int id)
         {
-            var cartToDelete = _context.Carts.Find(id);
-            if (cartToDelete == null)
+            string userId = User.FindFirstValue("id");
+            User currentUser = _context.Users.Find(userId);
+            Cart cartToDelete = _context.Carts.Find(id);
+            if (cartToDelete == null || currentUser == null)
             {
                 return NotFound();
+            }
+            else if (cartToDelete.UserId != currentUser.Id)
+            {
+                return Unauthorized();
             }
             _context.Carts.Remove(cartToDelete);
             _context.SaveChanges();
